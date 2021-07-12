4d0c2b4f7194338564a0af5c119429090a8d87a4
rhysw97 <rhysw97@gmail.com>
2021-07-12 14:59:05 +0100

    started creating header component

--- Web Development/amazon-clone/my-app/src/App.js
+++ Web Development/amazon-clone/my-app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Home from './Home';
+import Header from './Header';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      <Home />
+    <div className="Header">
+      <Header/>
     </div>
     
   );
---
+++ Web Development/amazon-clone/my-app/src/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Header.css'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import AmazonLogo from './images/amazon-logo.png';
+import SearchIcon from '@material-ui/icons/Search';
+
+export default class Header extends Component {
+    render() {
+      return (
+        <div class="amazon-header">
+          <img class="amazon-logo" src={AmazonLogo}/>
+          <form class="search-wrapper">
+            <input type="text"></input>
+            <div class="search-icon-wrapper">
+            <SearchIcon class="search-icon"/>
+          </div>
+          </form>
+          <div class="icon-wrapper">
+            <ShoppingBasketIcon class="shopping-basket"/>
+          </div>
+        </div>
+      );
+    }
+}
